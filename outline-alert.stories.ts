595a5a040b1fb42e4f2f724fec8a442b4f649c59
Josh Mabry <jmabry@phase2technology>
2022-07-18 09:26:21 -0700

    updates example story

--- outline-alert.stories.ts
+++ outline-alert.stories.ts
@@ -1,15 +1,10 @@
 import config, {Template} from './generated/config'
 
-//** TODO: Create story:variant command to build these files */
 export const OutlineAlertVariant = Template.bind({});
-// @TODO: Figure out why this .args is throwing a type error
-// eslint-disable-next-line
-// @ts-ignore
 OutlineAlertVariant.args = {
   statusType: 'success',
   story: '<p>This is a success alert.</p>',
 }
 
-// Need to re-export the config for storybook
 export default config
 
